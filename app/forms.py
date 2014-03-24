bcb78469ae586d65ebf8d6d7ed33a3b4c9e71192
Kevin Byrne <kevinbyrnety@gmail.com>
2014-03-24 04:37:04 -0700

    added error messages to signup form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,6 @@
 from flask.ext.wtf import Form
 from flask import render_template
 from wtforms import TextField, PasswordField, DateField, validators
-#from wtforms.validators import Required
 
 class Login(Form):
 	email = TextField('E-mail',[validators.Required()])
@@ -9,13 +8,21 @@ class Login(Form):
 
 class SignUp(Form):
 	firstname = TextField('First Name',[validators.Required()])
+	
 	lastname = TextField('Last Name', [validators.Required()])
+
 	nickname = TextField('Nickname',[validators.Optional()])
+
 	email = TextField('E-mail',[validators.Required()])
-	dob = DateField('DOB',[validators.Required()],format='%d/%m/%Y')
+
+	dob = DateField('DOB',[validators.Required(message='Please enter DOB in the required form')],format='%d/%m/%Y')
+
 	hometown = TextField('Hometown',[validators.Optional()])
+
 	password1 = PasswordField('Password',[validators.Required(),
-	validators.EqualTo('password2',message='Passwords must match.')])
+	validators.EqualTo('password2',message='Passwords must match.'),
+	validators.Length(min=5,max=30,message='Password must be minimum of 5 characters')])
+
 	password2 = PasswordField('Confirm Password',[validators.Required()])
 
 class EditProf(Form):
